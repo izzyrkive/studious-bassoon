442a581f88a0d4365ee1c9b25045b4c0e27534ef
izzyrkive <119257706+izzyrkive@users.noreply.github.com>
2023-01-29 13:55:19 -0500

    Added JavaScript code to script file
    Created JavaScript code for application to function.

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
+// Displays today's time and date
+var currentDate = moment().format('dddd, MMM Do');
+$("#currentDay").html(currentDate);
 
+$(document).ready(function () {
+    // Function for saveBtn on click event
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Saves text to local storage
+        localStorage.setItem(time, text);
+    })
+   
+    function checkTime() {
+        var currentTime = moment().hour();
+
+        // Function to loop through the timeblocks
+        $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+            // If else statement to check time for color coding the timeblocks
+            if (timeBlock < currentTime) {
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+                $(this).addClass("past");
+            }
+            else if (timeBlock === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+    // Pull event description from local storage
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+    $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+    $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+    $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+    $("#hour-5 .description").val(localStorage.getItem("hour-5"));
+
+    checkTime();
+})
